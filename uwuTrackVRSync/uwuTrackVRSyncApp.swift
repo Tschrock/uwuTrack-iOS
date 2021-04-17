35623e1ec1b209f5b9ac2a99574b402a441e7bf4
Tyler Schrock <tschrock123@gmail.com>
2021-04-16 17:44:11 -0700

    Initial Commit

---
+++ uwuTrackVRSync/uwuTrackVRSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  uwuTrackVRSyncApp.swift
+//  uwuTrackVRSync
+//
+//  Created by Tyler Schrock on 4/16/21.
+//
+
+import SwiftUI
+
+@main
+struct uwuTrackVRSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
